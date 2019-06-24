3a67ce3f13c8fffff9faad8ec4659f2b2e553f77
Sachin Patil <sachin_patil2@persistent.co.in>
2019-06-24 19:24:13 +0530

    updated basic.js

--- Inheritance/basic.js
+++ Inheritance/basic.js
@@ -1,3 +1,8 @@
 console.log(Object); // -> ƒ Object() { [native code] }
 console.log(Array); // -> ƒ Array() { [native code] }
 console.dir(Function); // -> ƒ Function() { [native code] }
+
+function fn() {}
+console.log('typeof fn.prototype: ' + typeof fn.prototype);
+console.log('typeof Object.prototype: ' + typeof Object.prototype);
+console.log('typeof fn.prototype.__proto__: ' + typeof fn.prototype.__proto__);
