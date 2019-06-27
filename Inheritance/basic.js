256818a3f5d38800b8bf84baa2c993e093ab2989
Sachin Patil <sachin_patil2@persistent.co.in>
2019-06-27 18:48:24 +0530

    updated

--- Inheritance/basic.js
+++ Inheritance/basic.js
@@ -6,3 +6,15 @@ function fn() {}
 console.log('typeof fn.prototype: ' + typeof fn.prototype);
 console.log('typeof Object.prototype: ' + typeof Object.prototype);
 console.log('typeof fn.prototype.__proto__: ' + typeof fn.prototype.__proto__);
+console.log();
+
+function doSomething(){}
+doSomething.prototype.foo = "bar";
+var doSomeInstancing = new doSomething();
+doSomeInstancing.prop = "some value";
+console.log("doSomeInstancing.prop:      " + doSomeInstancing.prop);
+console.log("doSomeInstancing.foo:       " + doSomeInstancing.foo);
+console.log("doSomething.prop:           " + doSomething.prop);
+console.log("doSomething.foo:            " + doSomething.foo);
+console.log("doSomething.prototype.prop: " + doSomething.prototype.prop);
+console.log("doSomething.prototype.foo:  " + doSomething.prototype.foo);
